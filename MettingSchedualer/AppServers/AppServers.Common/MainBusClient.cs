aa6c5fdbeae0270a367f9510fc663f0eafd6eca7
Pinkhas Yair Bracha <bpini@octally.com>
2016-02-10 14:05:36 +0200

    mail busclient

---
+++ MettingSchedualer/AppServers/AppServers.Common/MainBusClient.cs
@@ -0,0 +1,76 @@
+﻿using Common.DataModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppServers.Common
+{
+    public class MainBusClient
+    {
+        private Socket m_Sender;
+        public void ConnectToMainBus(string ip, int port)
+        {
+            // Data buffer for incoming data.
+            byte[] bytes = new byte[1024];
+
+            // Connect to a remote device.
+            try
+            {
+
+                IPHostEntry ipHostInfo = Dns.Resolve(ip);
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+                m_Sender = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    m_Sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        m_Sender.RemoteEndPoint.ToString());
+
+                    
+
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public void SendPacket(BaseDataPacket pct)
+        {
+            MemoryStream mem = new MemoryStream();
+            pct.Serialize(mem);
+            m_Sender.Send(mem.ToArray());
+        }
+
+        public void Shutdown()
+        {
+            m_Sender.Shutdown(SocketShutdown.Both);
+            m_Sender.Close();
+        }
+    }
+}
--- MettingSchedualer/AppServers/MainBus/MainBus/MainListenr.cs
+++ MettingSchedualer/AppServers/MainBus/MainBus/MainListenr.cs
@@ -12,7 +12,119 @@ namespace AppServers.MainBus
 {
     internal class MainListenr
     {
-        public void StartListening(string ip,int port)
+        private class MainBusClient
+        {
+            public ServerType ServerType { get; set; }
+
+            private byte[] m_Buff = new byte[256];
+            private Socket m_Socekt;
+            public MainBusClient(Socket client)
+            {
+                m_Socekt = client;
+
+            }
+
+            private void SetupRecieveCallback(Socket sock)
+            {
+                try
+                {
+                    AsyncCallback recieveData = new AsyncCallback(OnRecievedData);
+                    sock.BeginReceive(m_Buff, 0, m_Buff.Length, SocketFlags.None, recieveData, sock);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Setup Recieve Callback failed!");
+                }
+            }
+
+            public byte[] GetRecievedData(IAsyncResult ar)
+            {
+                int nBytesRec = 0;
+                try
+                {
+                    nBytesRec = m_Socekt.EndReceive(ar);
+                }
+                catch { }
+                byte[] byReturn = new byte[nBytesRec];
+                Array.Copy(m_Buff, byReturn, nBytesRec);
+
+                /*
+                // Check for any remaining data and display it
+                // This will improve performance for large packets 
+                // but adds nothing to readability and is not essential
+                int nToBeRead = m_sock.Available;
+                if( nToBeRead > 0 )
+                {
+                    byte [] byData = new byte[nToBeRead];
+                    m_sock.Receive( byData );
+                    // Append byData to byReturn here
+                }
+                */
+                return byReturn;
+            }
+
+            private void OnRecievedData(IAsyncResult ar)
+            {
+                // Socket was the passed in object
+                Socket sock = (Socket)ar.AsyncState;
+
+                byte[] aryRet = this.GetRecievedData(ar);
+
+                // If no data was recieved then the connection is probably dead
+                if (aryRet.Length < 1)
+                {
+                    Console.WriteLine("Client {0}, disconnected", m_Socekt.RemoteEndPoint);
+                    m_Socekt.Close();
+
+                    return;
+                }
+            }
+        }
+
+        private Dictionary<ServerType, MainBusClient> m_Clients;
+        protected void OnConnectRequest(IAsyncResult ar)
+        {
+
+            Socket listener = (Socket)ar.AsyncState;
+            MainBusClient newConnection = new MainBusClient(listener.EndAccept(ar));
+            m_Clients.Add(newConnection.ServerType, newConnection);
+            string data = null;
+            byte[] bytes = new Byte[1024];
+
+            while (true)
+            {
+                bytes = new byte[1024];
+                int bytesRec = handler.Receive(bytes);
+
+                if (bytesRec > 0)
+                {
+                    break;
+                }
+            }
+
+            MemoryStream mem = new MemoryStream(bytes);
+            BaseDataPacket packet = new BaseDataPacket();
+            packet.DeSerialize(mem);
+
+
+            switch (packet.PType)
+            {
+                case PacketType.StartDAL:
+                    {
+                        Console.WriteLine("StartDAL");
+                        break;
+                    }
+                case PacketType.GetInitData:
+                    {
+                        Console.WriteLine("GetInitData");
+                        break;
+                    }
+            }
+
+
+        }
+
+        public void StartListening(string ip, int port)
         {
             string data = null;
             byte[] bytes = new Byte[1024];
@@ -29,52 +141,27 @@ namespace AppServers.MainBus
                 listener.Bind(localEndPoint);
                 listener.Listen(10);
 
-                while (true)
-                {
-                    Console.WriteLine("Waiting for a connection...");
-                    Socket handler = listener.Accept();
-                    data = null;
+                Console.WriteLine("Waiting for a connection...");
+                listener.BeginAccept(new AsyncCallback(OnConnectRequest), listener);
 
-                    while (true)
-                    {
-                        bytes = new byte[1024];
-                        int bytesRec = handler.Receive(bytes);
-                        
-                        if (bytesRec > 0)
-                        {
-                            break;
-                        }
-                    }
+                Console.WriteLine("Press Enter to exit");
+                Console.ReadLine();
+                Console.WriteLine("OK that does it! Screw you guys I'm going home...");
 
-                    MemoryStream mem = new MemoryStream(bytes);
-                    BaseDataPacket packet = new BaseDataPacket();
-                    packet.DeSerialize(mem);
 
-                    
-                    switch (packet.PType)
-                    {
-                        case PacketType.StartDAL:
-                            {
-                                Console.WriteLine("StartDAL");
-                                break;
-                            }
-                        case PacketType.GetInitData:
-                            {
-                                Console.WriteLine("GetInitData");
-                                break;
-                            }
-                    }
-
-                }
 
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
-
-            Console.WriteLine("\nPress ENTER to continue...");
-            Console.Read();
+            finally
+            {
+                // Clean up before we go home
+                listener.Close();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
 
         }
     }
